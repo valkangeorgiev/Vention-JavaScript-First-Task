3d1d9873b44ec474e134c4186ee040d1f6c95f46
VALKAN GEORGIEV <valkan.georgiev1@gmail.com>
2023-12-18 20:43:59 +0200

    Demonstration of If/else statement

---
+++ first_exercise.js
@@ -0,0 +1,17 @@
+
+let personAge = 1;
+
+if (studentAge >= 18){
+
+    console.log('The person is an adult.')
+}
+
+else if (studentAge < 18 && studentAge > 12) {
+
+    console.log('The person is a teenager.')
+} 
+
+else {
+    
+    console.log('The person is a child.')
+}
