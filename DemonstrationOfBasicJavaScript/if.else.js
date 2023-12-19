04f5920626ce6aeb15ece64417f92b4b21bc393e
VALKAN GEORGIEV <valkan.georgiev1@gmail.com>
2023-12-19 09:53:57 +0200

    New Change

--- DemonstrationOfBasicJavaScript/if.else.js
+++ DemonstrationOfBasicJavaScript/if.else.js
@@ -1,12 +1,15 @@
 
-let ageOfSimona = 23;
+function SimonaAge() {
+    
+    let ageOfSimona = 23;
 
-if (ageOfSimona >= 18){
+    if (ageOfSimona >= 18){
     console.log('Simona is an adult.')
-}
-else if (studentAge < 18 && studentAge > 12) {
+        }
+    else if (studentAge < 18 && studentAge > 12) {
     console.log('Simona is a teenager.')
-} 
-else { 
+    } 
+    else { 
     console.log('Simona is a child.')
+    }
 }
